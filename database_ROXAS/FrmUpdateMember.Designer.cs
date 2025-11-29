0b3cbeb3c2949dac6ae3d34791920e4486646607
CAIRO <CAIRO@DESKTOP-G85462B>
2025-11-29 20:32:33 +0800

    DESIGN

--- database_ROXAS/FrmUpdateMember.Designer.cs
+++ database_ROXAS/FrmUpdateMember.Designer.cs
@@ -191,6 +191,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(334, 476);
             this.Controls.Add(this.cmbStudentId);
             this.Controls.Add(this.btnConfirm);
